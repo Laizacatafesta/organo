77d097f6180133424a60a1fe026adb7344a4f152
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-05 12:32:54 +0200

    add style

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,7 +1,11 @@
+import './Banner.css';
+
 function Banner() {
     //parece html, mas não é. É JSX, aqui o react faz o append no DOM. Transforma em elementos no DOM. É igual o document.createELemente(imh)
     return (
-        <img src="/imagens/banner.png" alt="Banner principal do Organo"/>
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner principal do Organo"/>;
+        </header>
     )
 };
 
