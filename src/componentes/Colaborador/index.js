5c1d7371aa42f38d8b9c28e37cb3051740387857
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-12 14:34:21 +0200

    add style ao colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='http://github.com/laizacatafesta.png' alt='Laiza Catafesta'/>
+            </div>
+            <div className='rodape'>
+                <h4>Laiza Catafesta</h4>
+                <h5>Desenvolvedora</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Times/index.js
+++ src/componentes/Times/index.js
@@ -1,9 +1,11 @@
+import Colaborador from '../Colaborador'
 import './Times.css'
 
 const Time = (props) => {
     return( 
         <section className='Time' style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            
         </section>
     )
 }
