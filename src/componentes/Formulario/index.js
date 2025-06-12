23c24534f0a7a1c7ba01352572e4e56901febf01
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-12 14:05:50 +0200

    elevado a lista de times

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -52,7 +52,7 @@ const Formulario = (props) => {
                 <ListaSuspensa 
                 obrigatorio={true} 
                 label="Time" 
-                itens={times}
+                itens={props.times}
                 valor={time}
                 aoAlterado={valor => setTime(valor)}/>
 
